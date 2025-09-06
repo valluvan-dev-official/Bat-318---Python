750abc48ac38a5499ca79615560972b0a250b9ab
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-09-06 12:25:52 +0530

    Add comprehensive string manipulation examples and documentation

---
+++ strings.py
@@ -0,0 +1,58 @@
+# Strings :
+
+# name = '' or "" or """ """
+
+
+
+# Strings are arrays :
+
+x = "Hello-World-welcome"
+
+# print(x[0])
+
+# Find a length :
+
+# print(len(x))
+
+
+# String Modifications :
+
+"""
+lower() :
+
+    convert to lowercase
+
+upper() :
+
+    convert to uppercase
+
+strip()
+
+    remove whitespace
+
+split()
+
+    split a string
+
+replace()
+
+    replace a value
+
+"""
+
+# Examples :
+
+"""
+print(x.lower())
+
+print(x.upper())
+
+print(x)
+
+print(x.strip())
+
+print(x.split("W"))
+
+print(x.replace('welcome','Thank You'))
+
+"""
