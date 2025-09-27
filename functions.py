f362962ae791765728a9ad2ffb11848df65aa649
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-09-27 11:08:00 +0530

    Refactor function definitions and enhance documentation in functions.py

--- functions.py
+++ functions.py
@@ -55,14 +55,165 @@ parameters - optional, it is a variable that is passed to the function
 
 """
 
-def myfun(name):
-
-    print("Welcome to Pyton - ",name)
+# Void Function :
 
 
+# Without Argument :
 
-myfun("Varun")
+"""
+def myfun():
 
-myfun("Monisha")
+    a = 10
 
-myfun("Jeeva")
+    b = 20
+
+    c = a + b
+
+    print(" a -- ",a)
+    print(" b -- ",b)
+
+    print(" Addition of a + b is = ",c)
+
+
+print(myfun())
+
+"""
+
+# With Argument :
+
+
+"""
+def myfun(a,b):
+
+    c = a + b 
+
+    print("C is -- ",c)
+
+
+
+x = int(input("Enter num : "))
+
+y = int(input("Enter num : "))
+
+
+myfun(20,30)
+
+print(myfun(x,y))
+
+myfun(100,200)
+
+"""
+
+
+# Non Void :
+
+# without argument :
+
+"""
+def myfun():
+
+    a = 10
+
+    b = 30
+
+    c = a + b 
+
+    return c
+
+
+print(myfun())
+
+"""
+
+# With Argument :
+
+"""
+def Myfun(x,y):
+
+    num = x + y 
+
+    return num
+
+
+a = 30
+
+b = 3
+
+print(Myfun(a,b))
+
+"""
+
+
+# ========= Returning Multiple Values : ===========
+
+
+"""
+def calculate(a,b):
+
+    addition = a + b
+
+    subtraction = a - b 
+
+    Multiply = a * b 
+
+    return addition,subtraction,Multiply
+
+
+print(calculate(20,5))
+
+add_,sub_,mul_ = calculate(20,5) # (25,25,100)
+
+
+print(add_)
+print(sub_)
+print(mul_)
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
