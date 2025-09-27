2c33906df08d037644105e1f170c81f9f5b74b70
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-09-27 13:00:44 +0530

    Add documentation for lambda functions with syntax and usage examples

---
+++ lambda_functions.py
@@ -0,0 +1,27 @@
+# Lambda Functions :
+
+"""
+A lambda function is a small anonymous function.
+Used for short, single-expression functions.
+
+Syntax :
+
+    lambda arguments : expression
+
+
+lambda: Keyword to define a lambda function.
+
+arguments: Variables that are passed to the function.
+
+expression: A single expression that is evaluated and returned by the lambda
+    function.
+"""
+
+
+# square = lambda x : x * x 
+
+# print(square(10))
+
+mul = lambda a : a * 2
+
+print(mul(10))
