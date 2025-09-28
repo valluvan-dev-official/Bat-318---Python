70cea67846b209255f74a1186283163e167ada6b
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2025-09-28 13:25:20 +0530

    Add initial implementation of module with addition function and sample usage in main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+import module
+
+
+# print("Welcome",module.name,"and your Salary is",module.salary)
+
+module.addition(10,50)
+
+"""
+
+"""
+from module import *
+
+print(name)
+
+print(salary)
+
+addition(35,40)
+
+"""
+
+
+from module import name,salary,addition
+
+
+print(name)
+
+print(salary)
+
+addition(10,10)
+
+# print(age)
---
+++ module.py
@@ -0,0 +1,14 @@
+name = "Varun"
+
+age = 25
+
+salary = 30000
+
+
+def addition(a,b):
+
+    c = a + b 
+
+    print("a is : ",a)
+    print("b is : ",b)
+    print("Total of a + b is : ",c)
